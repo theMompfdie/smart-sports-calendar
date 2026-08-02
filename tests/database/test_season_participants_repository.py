f3320871fdaae386b1c67c5e93e9ccbaf1cb1801
The Mompfdie <61227831+theMompfdie@users.noreply.github.com>
2026-08-02 11:53:25 +0200

    style: match project import convention

--- tests/database/test_season_participants_repository.py
+++ tests/database/test_season_participants_repository.py
@@ -2,7 +2,6 @@ import sqlite3
 from pathlib import Path
 
 import pytest
-
 from app.database.competitions_repository import CompetitionsRepository
 from app.database.database import Database
 from app.database.participants_repository import ParticipantsRepository
